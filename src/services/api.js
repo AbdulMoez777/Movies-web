6950f3045536263ea3a2a37d76b95df2c404ea1f
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-03-20 08:02:12 +0500

    5: Create API File and Making Popular Movies and Search Movies Function

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+const API_KEY = "edcd665824177017b8bdb4040b39e51f"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+
+export const getPopularMovies = async () => {
+    const response = await fetch (`${BASE_URL}/movies/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+
+};
+export const searchMovies = async (query) => {
+    const response = await fetch (`${BASE_URL}/search/popular?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json()
+    return data.results
+
+};
+
